688530611c4cf40b0fee8f0768b9c9d0f860b4f7
Harry Smith <sharry@seas.upenn.edu>
2023-04-19 13:17:07 -0400

    improves testing coverage

--- test/WorstComparatorEverTest.java
+++ test/WorstComparatorEverTest.java
@@ -9,6 +9,12 @@ class WorstComparatorEverTest {
     @Test
     void compare() {
         Comparator<Integer> tester = new WorstComparatorEver<>();
-        assertEquals(1, tester.compare(4, 5));
+        assertEquals(-1, tester.compare(4, 5));
+    }
+
+    @Test
+    void compareBetter() {
+        Comparator<Integer> tester = new WorstComparatorEver<>();
+        assertEquals(-1 * 1, tester.compare(4, 5));
     }
 }
